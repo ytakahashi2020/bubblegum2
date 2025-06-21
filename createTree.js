136f06414014a67b1798760759f32d59d319db34
ytakahashi2020 <y.takahashi2020@gmail.com>
2025-06-21 21:24:12 +0900

    Add tree fetching functionality to createTree.js

--- createTree.js
+++ createTree.js
@@ -1,6 +1,7 @@
 const { createUmi } = require('@metaplex-foundation/umi-bundle-defaults');
 const { generateSigner, keypairIdentity } = require('@metaplex-foundation/umi');
-const { createTreeV2 } = require('@metaplex-foundation/mpl-bubblegum');
+const { createTreeV2, fetchTreeConfigFromSeeds } = require('@metaplex-foundation/mpl-bubblegum');
+const { fetchMerkleTree } = require('@metaplex-foundation/mpl-account-compression');
 const { fromWeb3JsKeypair } = require('@metaplex-foundation/umi-web3js-adapters');
 const { Keypair } = require('@solana/web3.js');
 const fs = require('fs');
@@ -27,6 +28,26 @@ async function createBubblegumTree() {
 
   console.log('Tree created successfully!');
   console.log('Merkle Tree Address:', merkleTree.publicKey);
+
+  // Fetch Merkle Tree account
+  const merkleTreeAccount = await fetchMerkleTree(umi, merkleTree.publicKey);
+  console.log('\nMerkle Tree Account Info:');
+  console.log('Max Depth:', merkleTreeAccount.header.maxDepth);
+  console.log('Max Buffer Size:', merkleTreeAccount.header.maxBufferSize);
+  console.log('Authority:', merkleTreeAccount.header.authority);
+  console.log('Creation Slot:', merkleTreeAccount.header.creationSlot);
+  console.log('Sequence Number:', merkleTreeAccount.sequenceNumber);
+
+  // Fetch Tree Config account
+  const treeConfig = await fetchTreeConfigFromSeeds(umi, { merkleTree: merkleTree.publicKey });
+  console.log('\nTree Config Info:');
+  console.log('Tree Creator:', treeConfig.treeCreator);
+  console.log('Tree Delegate:', treeConfig.treeDelegate);
+  console.log('Total Capacity:', treeConfig.totalCapacity);
+  console.log('Number Minted:', treeConfig.numMinted);
+  console.log('Is Public:', treeConfig.isPublic);
+  console.log('Is Decompressible:', treeConfig.isDecompressible);
+  console.log('Version:', treeConfig.version);
 }
 
 createBubblegumTree().catch(console.error);
